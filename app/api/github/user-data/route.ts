d48c95c2a658358eb7c101d8d10e32df922b260d
woodbeary <lopez731@gmail.com>
2024-09-09 16:23:00 -0700

    fix build

--- app/api/github/user-data/route.ts
+++ app/api/github/user-data/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import { getServerSession } from "next-auth/next"
-import { authOptions } from '../auth/[...nextauth]/route'
+import { authOptions } from '../../auth/[...nextauth]/route'
 
 export async function GET() {
   const session = await getServerSession(authOptions)
