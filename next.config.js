f257168c53266c6f76d7f601d2d2daafb4282542
woodbeary <lopez731@gmail.com>
2024-09-09 13:32:55 -0700

    env

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
-console.log('Environment variables in next.config.js:');
-console.log('NEXTAUTH_URL:', process.env.NEXTAUTH_URL);
-console.log('TWITTER_CLIENT_ID exists:', !!process.env.TWITTER_CLIENT_ID);
-console.log('TWITTER_CLIENT_SECRET exists:', !!process.env.TWITTER_CLIENT_SECRET);
-console.log('NODE_ENV:', process.env.NODE_ENV);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = { fs: false, net: false, tls: false }
-    }
-    return config
-  },
 }
 
 module.exports = nextConfig
