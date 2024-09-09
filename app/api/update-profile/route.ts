4c30cf46ebd7d15ae85e3bfc4c8beb7f9672fe58
woodbeary <lopez731@gmail.com>
2024-09-09 16:25:10 -0700

    might be fixed by using lib

--- app/api/update-profile/route.ts
+++ app/api/update-profile/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import { getServerSession } from "next-auth/next"
-import { authOptions } from '../auth/[...nextauth]/route'
+import { authOptions } from '@/lib/auth'
 import { firestore } from "@/lib/firebaseAdmin"
 
 export async function POST(req: Request) {
