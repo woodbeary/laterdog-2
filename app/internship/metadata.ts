e40602fc821079147de4bd25812b42ea7e113800
woodbeary <lopez731@gmail.com>
2024-09-21 00:33:17 -0700

    forgot it

---
+++ app/internship/metadata.ts
@@ -0,0 +1,11 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Social Media Intern Wanted - later.dog',
+  description: 'Join later.dog as a Social Media Intern. Master the art of trash posting, engage with tech memes, and boost our online visibility!',
+  openGraph: {
+    title: 'Social Media Intern Opportunity at later.dog',
+    description: 'Master the art of trash posting, engage with tech memes, and boost our online visibility. Join us!',
+    type: 'website',
+  },
+};
